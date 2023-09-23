9878883be1d898c40de3e0ef9a68930044d7496d
Anthony Ohiero Lifu <tonylifu@outlook.com>
2023-09-23 16:14:09 +0100

    initial set up

---
+++ src/main/java/com/wsms/api/WsmsapiApplication.java
@@ -0,0 +1,13 @@
+package com.wsms.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WsmsapiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WsmsapiApplication.class, args);
+    }
+
+}
